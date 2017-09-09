2927dfd8e82e8fb0302528fdd923ed986b102e78
Jenn Lieu <jennlieu@MacBook-Pro.local>
2017-09-08 21:11:34 -1000

    Finished number 5

--- exercises.js
+++ exercises.js
@@ -100,9 +100,11 @@ console.log ("product:" + product);
  * Invoke the function and assign it to a variable named `quotient`.
  * Console.log `quotient` to test your code.
 */
-
-
-
+function divide (num1, num2) {
+	return num1 / num2
+}
+var quotient = divide (bango1, bango2); 
+console.log ("quotient:" + quotient);
 /*
  * #6
  * Function - checkSum
